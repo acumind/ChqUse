bc1c6963268ecb181fbd7131e3f9b170acb94c88
Gagan <gagan@tavant.com>
2014-01-08 23:17:36 +0530

    removed unused imports

--- src/in/cix/chquse/activity/ChqUseMainActivity.java
+++ src/in/cix/chquse/activity/ChqUseMainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
